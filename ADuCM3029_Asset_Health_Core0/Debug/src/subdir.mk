2078a92e360799232f173bbee73788cb4d5e65ab
Jiayuan Wang <Jiayuan.Wang@analog.com>
2020-04-13 13:25:59 -0400

    Initial Commit

---
+++ ADuCM3029_Asset_Health_Core0/Debug/src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/Communication.c \
+../src/Timer.c \
+../src/adxl372.c \
+../src/main.c \
+../src/platform_drivers.c 
+
+SRC_OBJS += \
+./src/Communication.o \
+./src/Timer.o \
+./src/adxl372.o \
+./src/main.o \
+./src/platform_drivers.o 
+
+C_DEPS += \
+./src/Communication.d \
+./src/Timer.d \
+./src/adxl372.d \
+./src/main.d \
+./src/platform_drivers.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: CrossCore GCC ARM Embedded C Compiler'
+	arm-none-eabi-gcc -Og -g -gdwarf-2 -ffunction-sections -fdata-sections -DCORE0 -D_DEBUG -DEVAL_ADICUP3029 -D_RTE_ -D__ADUCM3029__ -D__SILICON_REVISION__=0x100 @includes-4b29d3a71f493b1deff88665b3429812.txt -Wall -c -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
